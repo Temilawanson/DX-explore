ce2cb146c910dac80bf845defae13f52a66ddc2a
Temilawanson <105397963+Temilawanson@users.noreply.github.com>
2022-11-02 07:18:55 +0100

    Completed project for larger and smaller screens

---
+++ main.js
@@ -0,0 +1,10 @@
+
+
+var hamburgerdiv = document.querySelector(".hamburger")
+var mobilelinkholder = document.querySelector(".mobile-linkholder")
+
+
+function dosomething() {
+    hamburgerdiv.classList.toggle("showburger")
+    mobilelinkholder.classList.toggle("show-mobile-linkholder")
+}
